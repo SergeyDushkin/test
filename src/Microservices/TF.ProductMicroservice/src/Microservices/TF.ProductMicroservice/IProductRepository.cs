da6d8dfd046c830b4329295e7565fc6f05c7165e
Alex My <alexmy@outlook.com>
2016-02-22 20:22:31 +0300

    Product Microservice SLN
    Положил сюда. Имхо имеет смысл каждый микросервис держать в отдельном
    репе. Пока не допилил нормальную иерархию и не знаю, стоит ли сейчас на
    нее время тратить

---
+++ src/Microservices/TF.ProductMicroservice/src/Microservices/TF.ProductMicroservice/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TF.ProductMicroservice
+{
+    public interface IProductRepository
+    {
+        Product Get(Guid productId);
+        Product Save(Product product);
+        IEnumerable<Product> Get();
+        void Delete(Guid id);
+    }
+}
---
+++ src/Microservices/TF.ProductMicroservice/src/Microservices/TF.ProductMicroservice/ProductRepository.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace TF.ProductMicroservice
+{
+    public class ProductRepository : IProductRepository
+    {
+        public IEnumerable<Product> Get()
+        {
+            List<Product> result = null;
+            using (var productContext = new ProductContext())
+            {
+                result = productContext.Products.ToList();
+            }
+            if (result == null)
+            {
+                return new List<Product>();
+            }
+            return result;
+        }
+
+        public Product Get(Guid productId)
+        {
+            Product result = null;
+            using (var productContext = new ProductContext())
+            {
+                result = productContext.Products.Include("ChildProducts").SingleOrDefault(p => p.Id == productId);
+            }
+            return result;
+        }
+
+        public Product Save(Product product)
+        {
+            if (product.Id == Guid.Empty)
+            {
+                return Create(product);
+            }
+            else
+            {
+                return Update(product);
+            }
+        }
+
+        public void Delete(Guid productId)
+        {
+            Product toDel = null;
+            using (var productContext = new ProductContext())
+            {
+                toDel = productContext.Products.Include("ChildProducts").SingleOrDefault(p => p.Id == productId);
+                if (toDel != null)
+                {
+                    productContext.Products.Remove(toDel);
+                    productContext.SaveChanges();
+                }
+                else
+                {
+                    throw new ArgumentException();
+                }
+            }
+        }
+
+        private Product Update(Product product)
+        {
+            using (var productContext = new ProductContext())
+            {
+                productContext.Products.Attach(product);
+                productContext.Entry(product).State = EntityState.Modified;
+                productContext.SaveChanges();
+                return product;
+            }
+        }
+
+        private Product Create(Product product)
+        {
+            using (var productContext = new ProductContext())
+            {
+                var result = productContext.Products.Add(product);
+                productContext.SaveChanges();
+                return productContext.Products.Add(result);
+            }
+        }
+    }
+}
