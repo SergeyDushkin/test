28f8b9ea3669874a3c700033097410810cf0d89a
SergeyDushkin <leirbythe@gmail.com>
2016-03-01 15:50:32 +0300

    Add ClientUnitTest

---
+++ TF.MobileClient/TF.MobileClient/ClientUnitTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TF.MobileClient
+{
+    [TestClass]
+    public class ClientUnitTest
+    {
+        [TestMethod]
+        public void GetMainPageItemsTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void GetCatalogItemsTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void GetItemTest()
+        {
+
+        }
+    }
+}
