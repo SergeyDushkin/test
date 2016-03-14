a96bbf9ec4f1fe846071d252afff3417aa190270
SergeyDushkin <leirbythe@gmail.com>
2016-03-14 18:12:40 +0300

    add test

--- TF.MobileClient/TF.MobileClient/ClientUnitTest.cs
+++ TF.MobileClient/TF.MobileClient/ClientUnitTest.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+using System.Data.Services.Client;
+using System.Linq;
+
 namespace TF.MobileClient
 {
     [TestClass]
@@ -15,13 +18,31 @@ namespace TF.MobileClient
         [TestMethod]
         public void GetCatalogItemsTest()
         {
+            var container = new TF.Container(new Uri("http://localhost:5588/odata/"));
 
+            var data = container.CategoryTrees.ToList();
+
+            container.CategoryTrees.Single(r => r.Id == Guid.Parse("be2ebf01-4846-498f-92c5-33e8a3882a4d")).Products;
+
+            Assert.IsNotNull(data);
         }
 
         [TestMethod]
         public void GetItemTest()
         {
+            var container = new TF.Container(new Uri("http://localhost:5588/odata/"));
 
+            var data = container
+                .Products
+                .Where(r => r.Name == "WOK")
+                .ToList();
+
+            var alldata = container
+                .CreateQuery<TF.AggregateProduct>("Products")
+                .Execute();
+
+            Assert.IsNotNull(data);
+            Assert.IsNotNull(alldata);
         }
     }
 }
