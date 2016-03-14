92e366c42cc3383dd0913e336254b13e86a2f3ac
SergeyDushkin <leirbythe@gmail.com>
2016-03-14 22:41:33 +0300

    add file

---
+++ AggregateProductMicroservice/src/TF.AggregateProductMicroservice/Repositories/AggregateProductRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using TF.Data;
+
+namespace TF.AggregateProductMicroservice
+{
+    public class AggregateProductRepository : IAggregateProductProductRepository
+    {
+        private readonly IEnumerable<AggregateProduct> _data;
+
+        public AggregateProductRepository()
+        {
+            _data = new[] { 
+                new AggregateProduct()
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "WOK",
+                    Type = "REGULAR",
+                    Price = new Price { ProductPrice = 10}
+                }, 
+                new AggregateProduct()
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "WOK V2",
+                    Type = "REGULAR",
+                    Price = new Price { ProductPrice = 5}
+                }, 
+                new AggregateProduct()
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "NOODLE V2",
+                    Type = "KIT"
+                }, 
+                new AggregateProduct()
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "NOODLE V3",
+                    Type = "KIT"
+                }
+            };
+        }
+
+        public IEnumerable<AggregateProduct> Get()
+        {
+            return _data;
+        }
+    }
+}
